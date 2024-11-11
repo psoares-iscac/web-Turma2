ccbf82d93615d167acdf8993dfce6230cdccce83
Paulo Soares <127213462+psoares-iscac@users.noreply.github.com>
2024-11-11 13:20:01 +0000

    Add files via upload

---
+++ includes/menu.php
@@ -0,0 +1,22 @@
+ <!-- MENU PRINCIPAL -->
+ <nav class="navbar navbar-expand-md bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">WEB 20224</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="nav-menu">
+                <div class="navbar-nav ms-auto nav-underline">
+                    <a href="noticias.php" class="nav-link">Notícias</a>
+                    <a href="#" class="nav-link active">Opção 2</a>
+                </div>
+            </div>
+
+            <button type="button" class="btn">
+                <i class="bi bi-person-vcard" style="font-size: 1.5rem;"></i>
+            </button>    
+
+        </div>
+    </nav>
