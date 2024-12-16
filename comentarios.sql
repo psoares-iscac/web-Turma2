7878ddcd9c03a8b1c6ad34e9e285e8c8c2dc4d9e
psoares-iscac <psoares@iscac.pt>
2024-12-16 12:54:07 +0000

    4.1

--- comentarios.sql
+++ comentarios.sql
@@ -1,3 +1,13 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: turma2
+Target Host: localhost
+Target Database: turma2
+Date: 16/12/2024 12:52:16
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for comentarios
 -- ----------------------------
@@ -7,9 +17,9 @@ CREATE TABLE `comentarios` (
   `eventoId` int(10) unsigned NOT NULL DEFAULT 0,
   `email` varchar(250) DEFAULT '0',
   `mensagem` text NOT NULL,
-  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- ----------------------------
 -- Records 
@@ -17,4 +27,6 @@ CREATE TABLE `comentarios` (
 INSERT INTO `comentarios` VALUES ('1', '5', 'psoares@iscac.pt', 'Palestras e seminários muitio interessantes, com uma excelente organização. Recomendo vivamente a todos os que se preocupam com o seu bem estar.', '2024-07-23 00:00:00');
 INSERT INTO `comentarios` VALUES ('2', '5', 'Anónimo', 'Não gostei nada, foi uma autêntica vergonha. A organização falhou em muitos aspectos. Enormes filas para participar em qualquer uma das atividades.', '2024-08-10 00:00:00');
 INSERT INTO `comentarios` VALUES ('6', '5', 'Anónimo', 'srfhgsdfhdfh', '2024-12-12 17:58:07');
-
+INSERT INTO `comentarios` VALUES ('7', '5', 'psoares@iscac.pt', 'zdbgzcbxcnhb', '2024-12-12 17:58:07');
+INSERT INTO `comentarios` VALUES ('10', '5', 'p.paulo.soares@gmail.com', 'aqui vai', '0000-00-00 00:00:00');
+INSERT INTO `comentarios` VALUES ('11', '5', 'p.paulo.soares@gmail.com', 'OUTRO', '2024-12-16 12:51:54');
